05142091d9c0c77607d85b9cafb4006a9853829c
ToniRib <tonimarierib@gmail.com>
2016-01-26 16:40:51 -0700

    Update fog credentails

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :url =>':s3_domain_url',
+      :path => '/:class/:attachment/:id_partition/:style/:filename',
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
